7baef0966dec917559a67014c4f7f920b6a8b9ae
Zhengyang550 <18151521911@163.com>
2020-07-25 12:31:34 +0800

    Spring Boot监控中心客户端

---
+++ src/main/java/com/jnu/App.java
@@ -0,0 +1,16 @@
+package com.jnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zy
+ * @Description: 启动类
+ * @Date: 2020-2-8
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args){
+        SpringApplication.run(App.class,args);
+    }
+}
